b38623f0d87c1b74922d61dcd57e6b2aae2bb451
lucas cunha <lucasgalvao260@gmail.com>
2024-09-16 14:49:03 -0300

    feat: added the route file

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors"
+import route from "./routes"
 
 const app = express();
 
 app.use(express.json());
 app.use(cors)
+app.use(route)
 
 app.listen(3030, () => {
     console.log("Server running on port 3030")

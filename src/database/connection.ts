fef70d37c73fc98810d0cc0ac998e7482d37d287
lucas cunha <lucasgalvao260@gmail.com>
2024-09-16 16:16:14 -0300

    feat: added the mongoDB class for connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,29 +1,38 @@
 import * as dotenv from "dotenv"
 import { MongoClient, ServerApiVersion } from "mongodb";
 dotenv.config({ path: "./.env"});
-
-// const uri = "mongodb+srv://lucasgalvao:vqAEemxs76WHtdIj@cluster0.1g0jd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = process.env.MONGO_URI as string
-console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+class MongoDB {
+  private client!: MongoClient; 
+  private uri: string = process.env.MONGO_URI as string
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+  cosntructor() {
+    this.client = new MongoClient(this.uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });
   }
-}
-run().catch(console.dir);
+
+  public async connect() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await this.client.connect();
+      // Send a ping to confirm a successful connection
+      await this.client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await this.client.close();
+    }
+  }
+
+  public getClient() {
+    return this.client;
+  }
+
+}; 
+
+export const mongoDB = new MongoDB

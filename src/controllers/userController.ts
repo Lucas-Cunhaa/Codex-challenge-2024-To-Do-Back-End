0128bae68b3e386dfb1057db8f410e469ef0cf3e
lucas cunha <lucasgalvao260@gmail.com>
2024-09-17 14:52:38 -0300

    feat: added the bycrypt for passwordHash and more security

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from "express"
-import {insertUser, getUserByEmail ,getUserByEmailAndPassword} from "../database/userQueries"
+import { insertUser, getUserByEmail ,getUserByEmailAndPassword } from "../database/userQueries"
+import { hash } from "bcryptjs"
 import { UserDTO } from "../dtos/userDto"
+import bcrypt from 'bcryptjs';
+
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
-        const {name, gender, age, email, password,} = req.body
+        const {name, gender, age, email, password} = req.body
+
+        const hashedPassword = await hash(password, 6)
 
         const emailExists = await getUserByEmail(email)
         if (emailExists) return res.status(409).json({ message: "Email alredy exists", sucess: false })
 
-        const user = new UserDTO(name, gender, age, email, password)
+        const user = new UserDTO(name, gender, age, email, hashedPassword)
 
         const data = await insertUser(user)
 
@@ -19,17 +24,22 @@ export const registerUser = async (req: Request, res: Response) => {
     }
 }
 
-export const loginUser = async (req: Request, res: Response) => { 
+export const loginUser = async (req: Request, res: Response) => {
     try {
-        const { email, password } = req.body; 
-        if ( !email || !password) throw new Error("Invalid values provided for login");
+        const { email, password } = req.body;
 
-        const data = await getUserByEmailAndPassword(email, password);
+        if (!email || !password) return res.status(400).json({ message: "Invalid values provided for login", success: false });
 
-        if (data) return res.status(200).json({message: "Login successful", data :data?.insertedId, sucess: true})
+        const user = await getUserByEmail(email);
+        if (!user) return res.status(401).json({ message: "Invalid credentials", success: false });
         
-        res.status(401).json({message: "Invalid credentials", sucess: false})
+        const passwordMatch = await bcrypt.compare(password, user.password); 
+
+        if (!passwordMatch) return res.status(401).json({ message: "Invalid credentials", success: false });
+        
+        return res.status(200).json({ message: "Login successful", data: user._id, success: true });
     } catch (error) {
-        res.status(404).json({message: "Login operation error", sucess: false})
+        console.error("Error during login:", error);
+        return res.status(500).json({ message: "Login operation error", success: false });
     }
-}
+};
